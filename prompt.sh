e5746490096979b17c9f336533e1595455005d51
Motyak <m0tyak_@live.fr>
2025-07-24 23:03:36 +0200

    rollback to old

--- prompt.sh
+++ prompt.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# Current issue:
+# When a multi-line command is typed (with trailing `\`)
+# then the evaluation of PS0 overwrites the wrong line
+# (to fix, we should count the number of lines everytime
+# $PS2 is evaluated, and then move the cursor N lines above
+# in the evaluation of $PS0)
+
 [ "${BASH_SOURCE[0]}" == "$0" ] && {
     >&2 echo "this script is meant to be sourced, not executed"
     exit 1
 }
 
 function __setup_prompt {
-    # local DELIMIT='\[\]'
     local RESET='\[\e[0;0m\]'
     local BLACK='\[\e[0;90m\]'; END_BLACK=$RESET
     local BLUE='\[\e[0;34m\]'; END_BLUE=$RESET
@@ -14,27 +20,28 @@ function __setup_prompt {
     local SC='\[\e7\]' # save cursor pos
     local RC='\[\e8\]' # restore cursor pos
     local CUU1='\[\e[1A\]' # move cursor up 1 row
-    # local CUD1='\[\e[1B\]' # move cursor down 1 row
-    # local HPA1='\[\e[1G\]' # move cursor to column 1
 
     function __IN_EXIT_CODE_COLOR {
         local exit_code=$?
         local colored_text=$1
-        local RESET='\[\e[0;0m\]'
-        local RED='\[\e[0;31m\]'; END_RED=$RESET
-        local GREEN='\[\e[0;32m\]'; END_GREEN=$RESET
+        local RESET='\e[0;0m'
+        local RED='\e[0;31m'; END_RED=$RESET
+        local GREEN='\e[0;32m'; END_GREEN=$RESET
         if [ $exit_code -eq 0 ]; then
-            echo ''${GREEN}${colored_text}${END_GREEN}
+            printf ''${GREEN}${colored_text}${END_GREEN}
         else
-            echo ''${RED}${colored_text}${END_RED}
+            printf ''${RED}${colored_text}${END_RED}
         fi
     }
 
-    # function __register_line_above {
-    #     :
-    # }
+    # PS1='[\[$(__IN_EXIT_CODE_COLOR '\''\A'\'')\]'${BLACK}'.$(date +%S%3N)'${END_BLACK}' '${BLUE}'\W'${END_BLUE}']\$ '
+    # PS1='[$(__IN_EXIT_CODE_COLOR '\''\A'\'')'${BLACK}'.$(date +%S%3N)'${END_BLACK}' '${BLUE}'\W'${END_BLUE}']\$ '
 
-    PS1='['$(eval 'echo $(__IN_EXIT_CODE_COLOR '\''\A'\'')')${BLACK}'.$(date +%S%3N)'${END_BLACK}' '${BLUE}'\W'${END_BLUE}']\$ '
+    # PS1='[$(eval '\''echo $(__IN_EXIT_CODE_COLOR '\''\A'\'')'\'')'${BLACK}'.$(date +%S%3N)'${END_BLACK}' '${BLUE}'\W'${END_BLUE}']\$ '
+    # PS1='[$(__IN_EXIT_CODE_COLOR '\''\A'\'')'${BLACK}'.$(date +%S%3N)'${END_BLACK}' '${BLUE}'\W'${END_BLUE}']\$ '
+
+    PS1='[$(__IN_EXIT_CODE_COLOR '\''\A'\'')'${BLACK}'.$(date +%S%3N)'${END_BLACK}' '${BLUE}'\W'${END_BLUE}']\$ '
+
     PS0=''${SC}${CUU1}$(eval 'echo $PS1')${RC}
 }
 
