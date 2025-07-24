c8ce311a664aae16b248fbf0ee81ee5c21eeb30b
Motyak <m0tyak_@live.fr>
2025-07-24 21:40:27 +0200

    fix cur pos (bash escape sequences)

--- prompt.sh
+++ prompt.sh
@@ -7,26 +7,26 @@
 
 function __setup_prompt {
     # local DELIMIT='\[\]'
-    local RESET='\e[0;0m'
-    local BLACK='\e[0;90m'; END_BLACK=$RESET
-    local BLUE='\e[0;34m'; END_BLUE=$RESET
+    local RESET='\[\e[0;0m\]'
+    local BLACK='\[\e[0;90m\]'; END_BLACK=$RESET
+    local BLUE='\[\e[0;34m\]'; END_BLUE=$RESET
 
-    local SC='\e7' # save cursor pos
-    local RC='\e8' # restore cursor pos
-    local CUU1='\e[1A' # move cursor up 1 row
-    # local CUD1='\e[1B' # move cursor down 1 row
-    # local HPA1='\e[1G' # move cursor to column 1
+    local SC='\[\e7\]' # save cursor pos
+    local RC='\[\e8\]' # restore cursor pos
+    local CUU1='\[\e[1A\]' # move cursor up 1 row
+    # local CUD1='\[\e[1B\]' # move cursor down 1 row
+    # local HPA1='\[\e[1G\]' # move cursor to column 1
 
     function __IN_EXIT_CODE_COLOR {
         local exit_code=$?
         local colored_text=$1
-        local RESET='\e[0;0m'
-        local RED='\e[0;31m'; END_RED=$RESET
-        local GREEN='\e[0;32m'; END_GREEN=$RESET
+        local RESET='\[\e[0;0m\]'
+        local RED='\[\e[0;31m\]'; END_RED=$RESET
+        local GREEN='\[\e[0;32m\]'; END_GREEN=$RESET
         if [ $exit_code -eq 0 ]; then
-            printf ''${GREEN}${colored_text}${END_GREEN}
+            echo ''${GREEN}${colored_text}${END_GREEN}
         else
-            printf ''${RED}${colored_text}${END_RED}
+            echo ''${RED}${colored_text}${END_RED}
         fi
     }
 
@@ -34,15 +34,7 @@ function __setup_prompt {
     #     :
     # }
 
-    # PS1='[20:24'${BLACK}'.50040'${END_BLACK}' '${BLUE}'\W'${END_BLUE}']\$ '
-    # PS1='[\[$(__IN_EXIT_CODE_COLOR '\''\A'\'')'${BLACK}'\].$(date +%S%3N)\['${END_BLACK}'\] \['${BLUE}'\]\W\['${END_BLUE}'\]]\$ '
-    PS1='[\[$(__IN_EXIT_CODE_COLOR '\''\A'\'')'${BLACK}'.$(date +%S%3N)'${END_BLACK}' '${BLUE}'\W'${END_BLUE}']\$ '
-
-    # PS0=''${DELIMIT}${SC}${CUU1}$(eval 'echo $PS1')${RC}${DELIMIT}
-    # PS0=''${DELIMIT}${SC}${CUU1}'[                      ]$ '${RC}${DELIMIT}
-    # PS0=''${DELIMIT}${SC}${CUU1}'$(printf '\''[                      ]'\'')$ '${RC}${DELIMIT}
-    # PS0=''${DELIMIT}${CUU1}$(eval 'echo $PS1')${HPA1}${CUD1}${DELIMIT}
-
+    PS1='['$(eval 'echo $(__IN_EXIT_CODE_COLOR '\''\A'\'')')${BLACK}'.$(date +%S%3N)'${END_BLACK}' '${BLUE}'\W'${END_BLUE}']\$ '
     PS0=''${SC}${CUU1}$(eval 'echo $PS1')${RC}
 }
 
